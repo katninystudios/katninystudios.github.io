73d17df7bc6b86f665013bb84f9254eaf9db5388
katniny <okuwu1680@gmail.com>
2025-01-11 05:56:11 -0700

    Add products & all products

---
+++ assets/js/getTransSocialLastUpdated.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+   fetch("https://api.github.com/repos/katniny/transsocial")
+      .then((response) => response.json())
+      .then((json) => {
+         const time = json.updated_at;
+         const date = new Date(time);
+
+         // get hours and determine am/pm
+         const hours = date.getHours();
+         const amPm = hours >= 12 ? "PM" : "AM";
+
+         // convert hours to 12-hour format
+         const twelveHourFormat = hours % 12 || 12;
+
+         // format the date
+         const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().slice(-2)} at ${twelveHourFormat}:${date.getMinutes().toString().padStart(2, '0')} ${amPm}`;
+
+         // insert date
+         document.getElementById("lastUpdated").textContent = `Last Updated: ${formattedDate} (your time zone)`;
+      });
+});
